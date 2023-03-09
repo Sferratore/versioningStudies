e0297d285d98afccbe9b4763a9a1a881ce2e0aa0
Francesco Paolo Sferratore <sferratore.francescopaolo00@gmail.com>
2023-03-09 15:23:37 +0000

    instructions changed

--- cartellozza/programma.c
+++ cartellozza/programma.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-<<<<<<< HEAD
    printf("Hello, World!####");
-=======
-   printf("Hello, World!!!!!");
->>>>>>> 3f239e71c6c1075417fedab58e9c538a70c57458
    return 0;
 }
